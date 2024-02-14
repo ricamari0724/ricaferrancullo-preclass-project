2d7a21947ee750957e6d38faa026165ae1c988ba
Rica Mari <ricamari0724@gmail.com>
2024-02-14 16:34:10 +0800

    Create a script that displays your full name

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,9 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+def full_name()
+  names = Hash[first_name: "Rica", middle_name: "Zaragosa", last_name: "Ferrancullo"]
+  puts "#{names[:first_name]} ", "#{names[:middle_name]} ", "#{names[:last_name]}"
+end
+
+p full_name()
