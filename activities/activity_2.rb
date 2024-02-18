7f6abee5013eb57e0a2f0ac3206650383f7a66fc
Replit user <>
2024-02-18 19:25:58 +0000

    Activity 2: Create a script that displays your full name

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,9 +3,12 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
-def full_name()
-  names = Hash[first_name: "Rica", middle_name: "Zaragosa", last_name: "Ferrancullo"]
-  puts "#{names[:first_name]} ", "#{names[:middle_name]} ", "#{names[:last_name]}"
+def full_name(name)
+  names = {first_name: 'Rica', middle_name: 'Zaragosa', last_name:'Ferrancullo'}
+
+  names.each do |key, name|
+    puts "#{name}"
+  end
 end
 
-p full_name()
+p full_name('Rica', 'Zaragosa', 'Ferrancullo')
